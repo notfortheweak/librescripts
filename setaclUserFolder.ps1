4b87a424db1712e4d43e5c12225b3c1d97b026c7
notfortheweak <46659652+notfortheweak@users.noreply.github.com>
2026-03-27 18:39:22 -0500

    Change SetAccessRule to AddAccessRule

--- setaclUserFolder.ps1
+++ setaclUserFolder.ps1
@@ -8,5 +8,5 @@ $type = Read-Host "Allow or Deny"
 $fileSystemAccessRuleArgumentList = $identity, $fileSystemRights, $type
 $fileSystemAccessRule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $fileSystemAccessRuleArgumentList
 # Apply new rule
-$NewAcl.SetAccessRule($fileSystemAccessRule)
+$NewAcl.AddAccessRule($fileSystemAccessRule)
 Set-Acl -AclObject $NewAcl
