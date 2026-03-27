bb2da27af4263548be1f334aef420d51f179d46a
notfortheweak <46659652+notfortheweak@users.noreply.github.com>
2026-03-27 18:42:35 -0500

    Add PowerShell script to set ACL for a group

---
+++ setAclGroupFolder.ps1
@@ -0,0 +1,12 @@
+$NewAcl = Get-Acl
+#(Get-Acl -Path ").Access
+# Set properties
+$group = Read-Host "Enter group"
+$fileSystemRights = Read-Host "Read, ReadandExecute, Write, Modify, FullControl"
+$type = Read-Host "Allow or Deny"
+# Create new rule
+$fileSystemAccessRuleArgumentList = $group, $fileSystemRights, $type
+$fileSystemAccessRule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $fileSystemAccessRuleArgumentList
+# Apply new rule
+$NewAcl.AddAccessRule($fileSystemAccessRule)
+Set-Acl -AclObject $NewAcl
