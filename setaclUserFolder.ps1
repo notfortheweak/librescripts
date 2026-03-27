800e079b242d56c8370c34f1a16093d1f69fbb71
notfortheweak <46659652+notfortheweak@users.noreply.github.com>
2026-03-27 17:53:40 -0500

    Simplify Set-Acl command by removing path prompt
    Removed redundant folder path prompt for Set-Acl command.

--- setaclUserFolder.ps1
+++ setaclUserFolder.ps1
@@ -1,4 +1,4 @@
-$NewAcl = Get-Acl -Path Read-Host "Enter Folder Path"
+$NewAcl = Get-Acl
 #(Get-Acl -Path ").Access
 # Set properties
 $identity = Read-Host "Enter user"
@@ -9,4 +9,4 @@ $fileSystemAccessRuleArgumentList = $identity, $fileSystemRights, $type
 $fileSystemAccessRule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $fileSystemAccessRuleArgumentList
 # Apply new rule
 $NewAcl.SetAccessRule($fileSystemAccessRule)
-Set-Acl -Path Read-Host "Enter Folder Path" -AclObject $NewAcl
+Set-Acl -AclObject $NewAcl
