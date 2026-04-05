e65833a5b77b121e538909927724e1bda00599d5
notfortheweak <nftw@pm.me>
2026-04-05 03:16:16 -0500

    refactor

--- setACLGroupFolder.ps1
+++ setACLGroupFolder.ps1
@@ -1,11 +1,11 @@
 $NewAcl = Get-Acl
 #(Get-Acl -Path ").Access
 # Set properties
-$group = Read-Host "Enter group"
+$identity = Read-Host "Enter user"
 $type = Read-Host "Allow or Deny"
 $fileSystemRights = Read-Host "Read, ReadandExecute, Write, Modify, FullControl"
 # Create new rule
-$fileSystemAccessRuleArgumentList = $group, $fileSystemRights, $type
+$fileSystemAccessRuleArgumentList = $identity, $fileSystemRights, $type
 $fileSystemAccessRule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $fileSystemAccessRuleArgumentList
 # Apply new rule
 $NewAcl.AddAccessRule($fileSystemAccessRule)
