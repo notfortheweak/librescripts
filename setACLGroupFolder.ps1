033e70654d023c8ba85be28c61713a70ed2c7533
notfortheweak <nftw@pm.me>
2026-04-05 03:21:40 -0500

    updated variables and added new prompts for folder creation and group assignment in the newluseraddgrp.ps1 script.

--- setACLGroupFolder.ps1
+++ setACLGroupFolder.ps1
@@ -1,11 +1,11 @@
 $NewAcl = Get-Acl
 #(Get-Acl -Path ").Access
 # Set properties
-$identity = Read-Host "Enter user"
+$user = Read-Host "Enter user"
 $type = Read-Host "Allow or Deny"
 $fileSystemRights = Read-Host "Read, ReadandExecute, Write, Modify, FullControl"
 # Create new rule
-$fileSystemAccessRuleArgumentList = $identity, $fileSystemRights, $type
+$fileSystemAccessRuleArgumentList = $user, $fileSystemRights, $type
 $fileSystemAccessRule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $fileSystemAccessRuleArgumentList
 # Apply new rule
 $NewAcl.AddAccessRule($fileSystemAccessRule)
