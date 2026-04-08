5f2f239fc79cbb8d38aea10515626751596a4ba2
notfortheweak <46659652+notfortheweak@users.noreply.github.com>
2026-04-08 17:21:33 -0500

    Re-add AzureAD module import to profile

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -3,28 +3,3 @@ import-module PSWindowsUpdate
 import-module PackageManagement
 import-module NTFSSecurity
 import-module AzureAD
-
-function prompt {
-    $UserProf = $env:USERPROFILE -replace '[A-Za-z]:\\' -replace '([\\\.\(\)\{\}\?])','\$1'
-    $Location = "$(Get-Location)" -replace "([A-Za-z][:\$]\\)$UserProf",'$1~' -replace '(?<=^[A-Za-z\~]:{0,1}\\([^\\~]+\\){2})([^\\~]+\\)+(?=[^\\]+\\[^\\]+$)','..\' -replace '(?<=^)C:\\~','~'
-    $isAdmin  = [Security.Principal.WindowsPrincipal]::New(
-        [Security.Principal.WindowsIdentity]::GetCurrent()
-    ).IsInRole(
-        [Security.Principal.WindowsBuiltinRole]::Administrator
-    ) 
-    if($isAdmin){
-        $NameColor = "Red"
-    }else{
-        $NameColor = 'White'
-    }
-    $Domain = $env:UserDNSDomain, $env:UserDomain | 
-        Sort-Object Length -Descending | 
-        Select-Object -First 1
-    Write-Host -ForegroundColor $NameColor "$env:USERNAME@$Domain".toLower() -NoNewline
-    Write-Host -NoNewline ':'
-    Write-Host -ForegroundColor DarkCyan $Location -NoNewline
-    "`$$('>' * ($nestedPromptLevel)) "
-    $Host.UI.RawUI.WindowTitle = "$((Get-Location).Path)>"
-    "PS $($executionContext.SessionState.Path.CurrentLocation)> "
-    Write-Host ("[" + (Get-Date -Format "HH:mm:ss") + "] ") -NoNewline -ForegroundColor Cyan
-}
