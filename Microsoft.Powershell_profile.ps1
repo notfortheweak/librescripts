e62892af8889b845be0a0daf0fd4b8b5cebae53a
notfortheweak <46659652+notfortheweak@users.noreply.github.com>
2026-04-08 16:24:40 -0500

    Add PowerShell profile with custom prompt function

---
+++ Microsoft.Powershell_profile.ps1
@@ -0,0 +1,29 @@
+get-date
+
+import-module PSGit
+import-module PSWindowsUpdate
+import-module PackageManagement
+
+
+
+function prompt {
+    $UserProf = $env:USERPROFILE -replace '[A-Za-z]:\\' -replace '([\\\.\(\)\{\}\?])','\$1'
+    $Location = "$(Get-Location)" -replace "([A-Za-z][:\$]\\)$UserProf",'$1~' -replace '(?<=^[A-Za-z\~]:{0,1}\\([^\\~]+\\){2})([^\\~]+\\)+(?=[^\\]+\\[^\\]+$)','..\' -replace '(?<=^)C:\\~','~'
+    $isAdmin  = [Security.Principal.WindowsPrincipal]::New(
+        [Security.Principal.WindowsIdentity]::GetCurrent()
+    ).IsInRole(
+        [Security.Principal.WindowsBuiltinRole]::Administrator
+    ) 
+    if($isAdmin){
+        $NameColor = "Red"
+    }else{
+        $NameColor = 'White'
+    }
+    $Domain = $env:UserDNSDomain, $env:UserDomain | 
+        Sort-Object Length -Descending | 
+        Select-Object -First 1
+    Write-Host -ForegroundColor $NameColor "$env:USERNAME@$Domain".toLower() -NoNewline
+    Write-Host -NoNewline ':'
+    Write-Host -ForegroundColor DarkCyan $Location -NoNewline
+    "`$$('>' * ($nestedPromptLevel)) "
+}
